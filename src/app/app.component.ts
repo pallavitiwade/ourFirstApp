9811a9bf84f763a38fc0b2e48654eb02b919d9f1
pallavitiwade <tiwdepallavi97@gmail.com>
2026-04-11 18:55:43 +0530

    work on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit} from '@angular/core';
+import { ancientStructures, boardGames, famousAuthors, famousInventions, famousRivers, festivalsWorld, mountainRanges, movieGenres, musicalGenres, musicalInstrumentsWorld, prehistoricAnimals, spaceMissions, techInnovations, wondersOfNature, worldCuisines } from './consts/posts';
+import { Iancient, Iboard, Ifamous, Ifestive, Imountain, Imovie, Imusic, Iprehistoric, Ispace, Itech, Iworld } from './models/posts';
 
 @Component({
   selector: 'app-root',
@@ -7,90 +9,35 @@ import { Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'ourFirstApp';
-  skills:Array<string>=['HTML','CSS','JAVA','REACT']
-ngOnInit(): void {
-  this.skills.forEach(s=>{
-    console.log(s)
-  })
-}
 
-flowers:Array<string>=['daisy','jasmin','mogra','rose']
-ngOnInit1(): void {
-  this.flowers.forEach(f=>{
-    console.log(f)
-  })
-}
+
+
 
 fruits: Array<string> = ['Apple', 'Banana', 'Mango', 'Orange'];
+spaceArr:Array<Ispace>=spaceMissions
 
-ngOnInit2(): void {
-  this.flowers.forEach(f=>{
+worldCuisinesArr:Array<Iworld>=worldCuisines
+ mountainRangesArr:Array<Imountain>=mountainRanges
+ techInnovationsArr:Array<Itech>=techInnovations
+ famousRiversArr:Array<Ifamous>=famousRivers
+ famousAuthorsArr:Array<Ifamous>=famousAuthors
+ prehistoricAnimalsArr:Array<Iprehistoric>=prehistoricAnimals
+ famousInventionsArr:Array<Ifamous>=famousInventions
+ movieGenresArr:Array<Imovie>=movieGenres
+ musicalGenresArr:Array<Imusic>=musicalGenres
+ ancientStructuresArr:Array<Iancient>=ancientStructures
+ boardGamesArr:Array<Iboard>=boardGames
+ musicalInstrumentsWorldArr:Array<Imusic>=musicalInstrumentsWorld
+ festivalsWorldArr:Array<Ifestive>=festivalsWorld
+ wondersOfNatureArr:Array<Iworld>=wondersOfNature
+
+
+ngOnInit(): void {
+  this.boardGamesArr.forEach(f=>{
     console.log(f)
   })
 }
 
- countries: Array<string> = ['India', 'USA', 'Canada', 'Germany'];
-ngOnInit3(): void {
-  this.countries.forEach(f=>{
-    console.log(f)
-  })
-}
-
- movies:Array<string> = ['Inception', 'Avatar', 'Titanic', 'Joker'];
-ngOnInit4(): void {
-  this.movies.forEach(f=>{
-    console.log(f)
-  })
-}
-
- languages: Array<string> = ['Java', 'Python', 'C++', 'TypeScript'];
- ngOnInit5(): void {
-  this.languages.forEach(f=>{
-    console.log(f)
-  })
-}
-
-cities: Array<string> = ['Mumbai', 'Delhi', 'Pune', 'Hyderabad'];
- ngOnInit6(): void {
-  this.cities.forEach(f=>{
-    console.log(f)
-  })
-}
-
-animals: Array<string> = ['Dog', 'Cat', 'Lion', 'Elephant'];
- ngOnInit7(): void {
-  this.animals.forEach(f=>{
-    console.log(f)
-  })
-}
-days: Array<string>= ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
- ngOnInit8(): void {
-  this.days.forEach(f=>{
-    console.log(f)
-  })
-}
-tools: Array<string> = ['VS Code', 'Git', 'Postman', 'Chrome'];
-ngOnInit9(): void {
-  this.tools.forEach(f=>{
-    console.log(f)
-  })
-}
-
-
-months: Array<string>= ['January', 'February', 'March', 'April'];
-ngOnInit10(): void {
-  this.months.forEach(f=>{
-    console.log(f)
-  })
-}
-
-hobbies:Array<string>=['Reading', 'Gaming', 'Traveling'];
-ngOnInit11(): void {
-  this.hobbies.forEach(f=>{
-    console.log(f)
-  })
-}
-
+ 
 
 }
-
---
+++ src/app/models/posts.ts
@@ -0,0 +1,66 @@
+export interface Ispace{
+    title: string;
+    content: string;
+}
+
+export interface Iworld{
+    title: string;
+    content: string;
+}
+
+export interface Imountain{
+    title: string;
+    content: string;
+}
+export interface Ifamous{
+    title: string;
+    content: string;
+}
+
+export interface Iprehistoric{
+    title: string;
+    content: string;
+}
+
+export interface Ifamous{
+    title: string;
+    content: string;
+}
+
+
+export interface Imovie{
+    title: string;
+    content: string;
+}
+export interface Imusic{
+    title: string;
+    content: string;
+}
+export interface Itech{
+    title: string;
+    content: string;
+}
+export interface Ifamous{
+    title: string;
+    content: string;
+}
+export interface Iancient{
+    title: string;
+    content: string;
+}
+export interface Iboard{
+    title: string;
+    content: string;
+}
+export interface Imusic{
+    title: string;
+    content: string;
+}
+export interface Ifestive{
+    title: string;
+    content: string;
+}
+export interface Iwonder{
+    title: string;
+    content: string;
+}
