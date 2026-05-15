c524659880ddb5abb81b8e6502c05e16b85f9545
pallavitiwade <tiwdepallavi97@gmail.com>
2026-05-15 15:09:30 +0530

    initial pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ancientStructures, boardGames, famousAuthors, famousInventions, famousRivers, festivalsWorld, mountainRanges, movieGenres, musicalGenres, musicalInstrumentsWorld, prehistoricAnimals, spaceMissions, techInnovations, wondersOfNature, worldCuisines } from './consts/posts';
 import { Iancient, Iboard, Ifamous, Ifestive, Imountain, Imovie, Imusic, Iprehistoric, Ispace, Itech, Iworld } from './models/posts';
 import { Ianimal, Iapp, Ibands, Ibeverage, Icontinent, Idessert, Ifestival, Iflower, Igadget, Ihotel, Iinsect, Iisland, Ilandmark, Imineral, Imoon, Imounts, Iocean, Iplant, Iprofession, Iriver, Ispice, Itool, Iunive, Ivehicle } from './models/student';
@@ -6,50 +7,32 @@ import { animals, apps, beveragesMenu, continents, desserts, festivals, flowers,
 
 @Component({
   selector: 'app-root',
+  standalone:true,
+  imports:[CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'ourFirstApp';
+export class AppComponent {
+  title = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nisi culpa minus inventore similique veniam animi quia delectus nam at?'
+
+
+  course={
+    title:"Angular Development",
+    students:123456789,
+    rating:4.6,
+    price:199,
+    relaseDate:new Date(2025, 2 ,16)
+  }
 
 
 
 
-fruits: Array<string> = ['Apple', 'Banana', 'Mango', 'Orange'];
-spaceArr:Array<Ispace>=spaceMissions
-plantsArr:Array<Iplant>=planets
-mountainsArr:Array<Imounts>=mountains
-universitiesArr:Array<Iunive>=universities
-animalsArr:Array<Ianimal>=animals
-appsArr:Array<Iapp>=apps
-continentsArr:Array<Icontinent>=continents
-dessertsArr:Array<Idessert>=desserts
-oceansArr:Array<Iocean>=oceans
-gadgetsArr:Array<Igadget>=gadgets
-landmarksArr:Array<Ilandmark>=landmarks
-vehiclesModelsArr:Array<Ivehicle>=vehiclesModels
-professionsArr:Array<Iprofession>=professions
-beveragesMenuArr:Array<Ibeverage>=beveragesMenu
-flowersArr:Array<Iflower>=flowers
-hotelsArr:Array<Ihotel>=hotels
-islandsArr:Array<Iisland>=islands
-insectsArr:Array<Iinsect>=insects
-festivalsArr:Array<Ifestival>=festivals
-riversArr:Array<Iriver>= rivers
-spicesArr:Array<Ispice>=spices
-toolsArr:Array<Itool>=tools
-mineralsArr:Array<Imineral>=minerals
-planetsMoonsArr:Array<Imoon>=planetsMoons
-musicalBandsArr:Array<Ibands>=musicalBands
 
 
 
-ngOnInit(): void {
-  this.spaceArr.forEach(f=>{
-    console.log(f)
-  })
-}
+  }
+
 
  
 
-}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { TabComponent } from './tab/tab.component';
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent,
+    // TabComponent
   ],
   imports: [
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
